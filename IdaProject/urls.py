4332796e65319eae69674deaf0e8625cbc349011
Golhoper <dead3@bk.ru>
2019-05-19 18:05:38 +0300

    fix static 2

--- IdaProject/urls.py
+++ IdaProject/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from UploadImage.views import MainPage, UploadPage, ShowImage
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
 
@@ -9,4 +10,4 @@ urlpatterns = [
     path('upload/', UploadPage.as_view(), name="upload"),
     re_path(r'^image/(?P<pk>\w+)/$', ShowImage.as_view(), name="image_page"),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + staticfiles_urlpatterns()
